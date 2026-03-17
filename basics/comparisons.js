138600075c37bd0688d52a21ce3110e3ffcaff1e
H-2006 <sawantharshal013@gmail.com>
2026-03-17 16:03:23 +0000

    comparisons.js

---
+++ basics/comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); //js checks the values in string not the type of variable //true
+// console.log("ram" > 1); //false
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+// // === 
+
+console.log("2" === 2); //checks the value and datatype is equal or not //false
+console.log("2" === "2"); //true
